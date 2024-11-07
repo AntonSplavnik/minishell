46c8967fa53b912bc4b0bbfd322f7fbff2e7af8f
AudreyBil <15158467+AudreyBil@users.noreply.github.com>
2024-11-07 12:57:22 +0100

    readline & add history set-up in main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 17:02:10 by abillote          #+#    #+#             */
-/*   Updated: 2024/11/05 17:07:03 by abillote         ###   ########.fr       */
+/*   Updated: 2024/11/07 11:54:33 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 int	main(void)
 {
+	char	*input;
+
+	while (1)
+	{
+		input = readline("minishell$ ");
+		clean_input(input);
+		//add input to history if it's not empty
+		if (input[0] != '\0')
+		{
+			add_history(input);
+		}
+	}
 	return (0);
 }
