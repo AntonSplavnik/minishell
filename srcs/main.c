8983a20090deaf29052a37665332552981275dfe
Audrey Billoteau <abillote@c1b1c3.42berlin.de>
2024-11-07 14:18:01 +0100

    adding comments for clarity

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 17:02:10 by abillote          #+#    #+#             */
-/*   Updated: 2024/11/07 11:54:33 by abillote         ###   ########.fr       */
+/*   Updated: 2024/11/07 14:17:33 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	main(void)
 
 	while (1)
 	{
+		//store input with readline
 		input = readline("minishell$ ");
-		clean_input(input);
 		//add input to history if it's not empty
 		if (input[0] != '\0')
 		{
