2e77d3227d6c468351a0d35e618734f0cdc894e6
AudreyBil <15158467+AudreyBil@users.noreply.github.com>
2024-11-11 11:29:31 +0100

    new struct for tokenisation

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 17:03:20 by abillote          #+#    #+#             */
-/*   Updated: 2024/11/07 17:00:59 by abillote         ###   ########.fr       */
+/*   Updated: 2024/11/11 11:24:30 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,25 @@
 /*Include libft*/
 # include "../libft/libft.h"
 
+typedef enum e_token_type
+{
+	TOKEN_WORD, //all regular words or arguments
+	TOKEN_COMMAND, //built-in commands
+	TOKEN_QUOTES, // quotes ""
+} t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*content;
+	struct s_token	*next;
+}			t_token;
+
 typedef struct s_command
 {
 	char	**args;
 	char	*function;
-	char	**splitted_input;
+	t_token	*tokens;
 }			t_command;
-
-typedef struct s_tokens
-{
-	char			*type;
-	char			*content;
-	// struct s_tokens	*next;
-}			t_tockens;
 
 #endif
