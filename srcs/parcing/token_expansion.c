78626aff79e7116de917f89f766e387f4de9d38a
AudreyBil <15158467+AudreyBil@users.noreply.github.com>
2024-11-29 17:43:13 +0100

    Fixed: env var expansion working - not norminette proofed

--- srcs/parcing/token_expansion.c
+++ srcs/parcing/token_expansion.c
@@ -6,7 +6,7 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 14:43:49 by abillote          #+#    #+#             */
-/*   Updated: 2024/11/28 18:29:49 by abillote         ###   ########.fr       */
+/*   Updated: 2024/11/29 17:37:20 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,23 +55,41 @@ void	get_var_length(const char *str, t_env *env_list, size_t *i, size_t *expande
 	*expanded_len += ft_strlen(var_value);
 	*i += ft_strlen(var_name);
 	free(var_name);
-	free(var_value);
 }
 
 /*Get the env var value. For $?, need to figure out how to store it in the shell struct to return it
 If no var existing, return an empty string*/
-void expand_var(const char *str, t_env *env_list, size_t *i, char **expanded)
-{
-	char *var_name;
-	char *var_value;
-	size_t j;
+//void expand_var(const char *str, t_env *env_list, size_t *i, char *expanded)
+//{
+//	char *var_name;
+//	char *var_value;
+//	size_t j;
 
-	var_name = get_var_name(str);
-	var_value = get_var_value(env_list, var_name);
-	j = ft_strlen(*expanded);
-	ft_strcpy(*expanded + j, var_value);
-	*i += ft_strlen(var_name) + 1;
-	free(var_name);
+//	var_name = get_var_name(str);
+//	var_value = get_var_value(env_list, var_name);
+//	j = ft_strlen(expanded);
+//	ft_strcpy(expanded + j, var_value);
+//	ft_strlcat(expanded, var_value, ft_strlen(expanded) + ft_strlen(var_value) + 1);
+//	printf("expanded: %s\n", expanded);
+//	*i += ft_strlen(var_name) + 1;
+//	free(var_name);
+//}
+
+void expand_var(const char *str, t_env *env_list, size_t *i, char *expanded)
+{
+    char *var_name;
+    char *var_value;
+    size_t len;
+
+    var_name = get_var_name(str);
+    var_value = get_var_value(env_list, var_name);
+    len = 0;
+    while (expanded[len] && len < ft_strlen(expanded))
+        len++;
+    ft_strcpy(expanded + len, var_value);
+	printf("expanded: %s\n", expanded);
+    *i += ft_strlen(var_name) + 1;
+    free(var_name);
 }
 
 void	init_expansion_params(size_t *i, size_t *j, int *in_squote, int *in_dquote)
@@ -140,6 +158,7 @@ void fill_token_content_after_expansion(t_token *token, char *expanded, t_env *e
 	in_dquote = 0;
 	i = 0;
 	j = 0;
+	expanded[j] = '\0';
 	while (token->content[i])
 	{
 		if ((token->content[i] == '\'' && !in_dquote) || \
@@ -152,7 +171,11 @@ void fill_token_content_after_expansion(t_token *token, char *expanded, t_env *e
 			i++;
 		}
 		else if (token->content[i] == '$' && !in_squote && token->content[i + 1])
-			expand_var(token->content + i + 1, env_list, &i, &expanded);
+		{
+			expanded[j] = '\0';
+			expand_var(token->content + i + 1, env_list, &i, expanded);
+			j = ft_strlen(expanded);
+		}
 		else
 			expanded[j++] = token->content[i++];
 	}
