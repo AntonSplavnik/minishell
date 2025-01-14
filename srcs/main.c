e344e1b0a86d7e65b9b778cfd49fb2b1af0b2810
AudreyBil <15158467+AudreyBil@users.noreply.github.com>
2025-01-14 12:21:16 +0100

    Enhanced: syntax error when delimiter is at end of command

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 17:02:10 by abillote          #+#    #+#             */
-/*   Updated: 2025/01/10 17:40:22 by abillote         ###   ########.fr       */
+/*   Updated: 2025/01/14 12:19:06 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	main(int argc, char **argv, char **env)
 	t_shell	*s;
 	char	*args;
 	t_error	error;
+	t_token	*last;
 
 	(void) argv;
 	s = NULL;
@@ -58,6 +59,15 @@ int	main(int argc, char **argv, char **env)
 		}
 		//split input into tokens
 		error = input_to_token(&s->token_list, args);
+		last = get_last_token(s->token_list);
+		if (last && (last->type == TYPE_HEREDOC_OP || last->type == TYPE_PIPE || last->type == TYPE_REDIRAPPEND || \
+		last->type == TYPE_REDIRIN || last->type == TYPE_REDIROUT))
+		{
+			error = ERR_PARCING;
+			handle_error_free_tokens(error, &s->token_list, args);
+			s->token_list = NULL;
+			continue ;
+		}
 		if (error != SUCCESS)
 		{
 			free(args);
@@ -71,6 +81,7 @@ int	main(int argc, char **argv, char **env)
 		if (error != SUCCESS)
 		{
 			handle_error_free_tokens(error, &s->token_list, args);
+			s->token_list = NULL;
 			s->exit_status = 2;
 			continue ;
 		}
--- srcs/parcing/token_extraction.c
+++ srcs/parcing/token_extraction.c
@@ -6,7 +6,7 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 14:54:55 by abillote          #+#    #+#             */
-/*   Updated: 2025/01/10 18:16:06 by abillote         ###   ########.fr       */
+/*   Updated: 2025/01/13 17:50:39 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,13 +52,20 @@ static char	*extract_delimiter_token(const char *str, size_t *i, t_error *error)
 	len = get_delimiter_len(str + *i);
 	token = ft_substr(str, *i, len);
 	if (!token)
+	{
 		*error = ERR_MALLOC;
+		return (NULL);
+	}
 	*i += len;
 	j = *i;
 	while (is_space(str[j]))
 		j++;
 	if (str[j] == '<' || str[j] == '>' || str[j] == '|')
+	{
 		*error = ERR_PARCING;
+		free(token);
+		return (NULL);
+	}
 	return (token);
 }
 
